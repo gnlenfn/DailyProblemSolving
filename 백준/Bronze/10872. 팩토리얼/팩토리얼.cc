1560ab27c55dd53c4ac02ff54647c03eba382b0d
Yun, Hong In <akenfn513@gmail.com>
2022-10-04 10:34:46 +0900

    [Bronze V] Title: 팩토리얼, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/10872. 팩토리얼/팩토리얼.cc
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int recur(int n) {
+    if (n == 0) return 1;
+
+    return recur(n - 1 ) * n;
+}
+
+int main() {
+    int a;
+    cin >> a;
+
+    cout << recur(a) << "\n";
+    
+    return 0;
+}
+
