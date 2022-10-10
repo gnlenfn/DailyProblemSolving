ae446f503c33319f3a1544cfecf269734a2a8161
Yun, Hong In <akenfn513@gmail.com>
2022-10-10 13:05:13 +0900

    [level 2] Title: JadenCase 문자열 만들기, Time: 0.01 ms, Memory: 4.17 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/12951. JadenCase 문자열 만들기/JadenCase 문자열 만들기.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+string solution(string s) {
+    string answer = "";
+    answer += toupper(s[0]);
+    for(int i = 1; i < s.size(); i++){
+        if(s[i - 1] == ' ') s[i] = toupper(s[i]);
+        else s[i] = tolower(s[i]);
+        answer += s[i];
+    }
+    
+    return answer;
+}
