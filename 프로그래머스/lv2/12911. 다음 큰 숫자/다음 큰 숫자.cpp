1e7a62a7628f7c7657454399cd783570f9ad998d
Yun, Hong In <akenfn513@gmail.com>
2022-10-13 20:48:26 +0900

    [level 2] Title: 다음 큰 숫자, Time: 0.01 ms, Memory: 3.83 MB -BaekjoonHub

--- 프로그래머스/lv2/12911. 다음 큰 숫자/다음 큰 숫자.cpp
+++ 프로그래머스/lv2/12911. 다음 큰 숫자/다음 큰 숫자.cpp
@@ -1,20 +1,9 @@
 #include <bits/stdc++.h>
 
 using namespace std;
-
-int cntOne(int n){
-    int cnt = 0;
-    while(n){
-        if(n % 2) cnt++;
-        n /= 2;
-    }
-    return cnt;
-}
-
 int solution(int n) {
-    int c = cntOne(n);
-    while(1){
-        n++;
-        if(c == cntOne(n)) return n;
-    }
+    int cnt = bitset<20>(n).count();
+    
+    while(bitset<20>(++n).count() != cnt);
+    return n;
 }
