6fe286b4d30cfb61c456e8b759e8ee2ad9675075
Yun, Hong In <akenfn513@gmail.com>
2022-10-19 16:57:22 +0900

    [level 2] Title: 구명보트, Time: 1.39 ms, Memory: 4.57 MB -BaekjoonHub

--- 프로그래머스/lv2/42885. 구명보트/README.md
+++ 프로그래머스/lv2/42885. 구명보트/README.md
@@ -1,10 +1,10 @@
 # [level 2] 구명보트 - 42885 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42885) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42885?language=cpp) 
 
 ### 성능 요약
 
-메모리: 10.5 MB, 시간: 7.68 ms
+메모리: 4.57 MB, 시간: 1.39 ms
 
 ### 구분
 
---
+++ 프로그래머스/lv2/42885. 구명보트/구명보트.cpp
@@ -0,0 +1,24 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int solution(vector<int> people, int limit) {
+    int answer = 0;
+    
+    sort(people.begin(), people.end());
+    
+    int l = 0, r = people.size() - 1;
+    
+    while(l <= r){
+        if(people[l] + people[r] <= limit){
+            answer++;
+            l++;
+            r--;
+        }
+        else{
+            answer++;
+            r--;
+        }
+    }
+    return answer;
+}
