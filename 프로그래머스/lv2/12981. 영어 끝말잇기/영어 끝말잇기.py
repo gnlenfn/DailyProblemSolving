09f4cdae298cf7f19bde5dd0a620b0b1c3969a8c
Yun, Hong In <akenfn513@gmail.com>
2022-12-07 18:13:37 +0900

    [level 2] Title: 영어 끝말잇기, Time: 0.08 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/lv2/12981. 영어 끝말잇기/영어 끝말잇기.py
+++ 프로그래머스/lv2/12981. 영어 끝말잇기/영어 끝말잇기.py
@@ -1,23 +1,11 @@
-from collections import defaultdict
-
 def solution(n, words):
-    num = defaultdict(int)
-    dic = []
-    
-    idx = 1
-    for word in words:
-        idx = idx % n
-        if not idx:
-            idx = n
+    for idx in range(1, len(words)):
+        if words[idx][0] != words[idx-1][-1]:
+            return [idx % n + 1, idx // n + 1]
         
-        if dic and word[0] != dic[-1][-1]:
-            return [idx, num[idx] + 1]
-        
-        if word in dic:
-            return [idx, num[idx] + 1]
-        
-        dic.append(word)
-        num[idx] += 1
-        idx += 1
+        if words[idx] in words[:idx]:
+            return [idx % n + 1, idx // n + 1]
         
     return [0, 0]
+            
+        
