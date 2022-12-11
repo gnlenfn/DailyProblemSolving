f2f1530807bb50b594f70acd052abeafbbed2df3
Yun, Hong In <akenfn513@gmail.com>
2022-12-11 20:35:02 +0900

    [level 2] Title: 예상 대진표, Time: 0.00 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/lv2/12985. 예상 대진표/예상 대진표.py
+++ 프로그래머스/lv2/12985. 예상 대진표/예상 대진표.py
@@ -1,25 +1,13 @@
 def solution(n,a,b):
     answer = 0
     
-    def partner(n):
-        if n % 2:
-            return n + 1    
-        return n - 1
-    
-    def get_even(n):
-        if n % 2:
-            return n + 1
-        return n
-
-    while True:
-        answer += 1
-        
-        if partner(a) == b:
+    while True:       
+        if a == b:
             return answer
         
-        a = get_even(a) // 2
-        b = get_even(b) // 2
-        
+        a = (a + 1) // 2
+        b = (b + 1) // 2
+        answer += 1
 
     return answer
 
