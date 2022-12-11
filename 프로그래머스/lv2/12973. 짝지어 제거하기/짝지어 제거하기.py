1ab463fd257291b83acf23525a51a2ac0306f840
Yun, Hong In <akenfn513@gmail.com>
2022-12-11 20:25:04 +0900

    [level 2] Title: 짝지어 제거하기, Time: 146.60 ms, Memory: 14.7 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/12973. 짝지어 제거하기/짝지어 제거하기.py
@@ -0,0 +1,18 @@
+def solution(s):
+    stack = []
+    for char in s:
+        if not stack:
+            stack.append(char)
+        
+        elif stack[-1] == char:
+            stack.pop()
+        
+        else:
+            stack.append(char)
+            
+    if stack:
+        return 0
+    
+    return 1
+        
+            
