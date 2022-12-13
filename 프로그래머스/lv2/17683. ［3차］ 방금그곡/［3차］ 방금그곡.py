9f394a07a0d9cc65017fa8d43d84e7e9e8e61202
Yun, Hong In <akenfn513@gmail.com>
2022-12-13 22:07:10 +0900

    [level 2] Title: [3차] 방금그곡, Time: 7.23 ms, Memory: 10.4 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/17683. ［3차］ 방금그곡/［3차］ 방금그곡.py
@@ -0,0 +1,39 @@
+def solution(m, musicinfos):
+    answer = []
+    find_notes = transform(m)
+    for idx, info in enumerate(musicinfos):
+        start, end, title, codes = info.split(",")
+        duration = get_duration(start, end)
+        codes = transform(codes)
+
+        if duration <= len(codes):
+            played = codes[:duration]
+        else:
+            rep = duration // len(codes)
+            played = codes * (rep + 1)
+            played = played[:duration]
+        
+        if find_notes in played:
+            answer.append((duration, idx, title))
+    
+    if answer:
+        answer.sort(key=lambda x : (-x[0], x[1]))
+        return answer[0][2]
+    
+    return "(None)"
+
+def get_duration(start, end):
+    start_h, start_m = map(int, start.split(":"))
+    end_h, end_m = map(int, end.split(":"))
+    
+    hours = end_h - start_h
+    mins = 60 * hours + end_m - start_m
+    return mins
+
+def transform(codes):
+    while codes.find("#") > 0:
+        idx = codes.index("#")
+        codes = codes[:idx-1] + codes[idx-1].lower() + codes[idx+1:]
+        
+    return codes
+    
