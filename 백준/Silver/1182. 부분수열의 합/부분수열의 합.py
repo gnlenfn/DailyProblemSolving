5cdc3c9ff25a9913f3b8d43854eec867abdcc8ae
Yun, Hong In <akenfn513@gmail.com>
2022-12-20 13:43:43 +0900

    [Silver II] Title: 부분수열의 합, Time: 240 ms, Memory: 30616 KB -BaekjoonHub

--- 백준/Silver/1182. 부분수열의 합/부분수열의 합.py
+++ 백준/Silver/1182. 부분수열의 합/부분수열의 합.py
@@ -4,10 +4,18 @@ n, s = map(int, input().split())
 nums = list(map(int, input().split()))
 answer = 0
 
-for i in range(1, len(nums) + 1):
-    candidates = combinations(nums, i)
-    for cand in candidates:
-        if sum(cand) == s:
+def recur(k, value):
+    if k == n:
+        global answer
+        if value == s:
             answer += 1
+    
+    else:
+        recur(k + 1, value + nums[k])
+        recur(k + 1, value)
 
+recur(0, 0)
+if s == 0:
+    answer -= 1
+    
 print(answer)
