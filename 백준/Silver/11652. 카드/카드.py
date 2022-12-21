db6680e44366cc1474f71a51b39371c93077a59d
Yun, Hong In <akenfn513@gmail.com>
2022-12-21 15:42:09 +0900

    [Silver IV] Title: 카드, Time: 148 ms, Memory: 37664 KB -BaekjoonHub

--- 백준/Silver/11652. 카드/카드.py
+++ 백준/Silver/11652. 카드/카드.py
@@ -7,7 +7,19 @@ nums = []
 for _ in range(n):
     nums.append(int(sys.stdin.readline()))
 
-ret = Counter(nums).most_common()
-ret.sort(key=lambda x: (-x[1], x[0]))
+nums.sort()
+mode = nums[0]   # 최빈값 초기화
+mode_count = 1   # 최빈값 카운트
+current = 1      # 현재 수 카운트
 
-print(ret[0][0])
+for i in range(1, n):
+    if nums[i] == nums[i-1]:  # 같은 수면 카운트 증가
+        current += 1
+    else:
+        current = 1           # 다른 수면 처음부터 카운트
+    
+    if mode_count < current:  # 최빈값 보다 현재값 카운트가 크면
+        mode_count = current  # 최빈값 갱신
+        mode = nums[i]
+
+print(mode)
