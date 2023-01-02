5b57df91949534eed133b9f2566ac452d2db072e
Yun, Hong In <akenfn513@gmail.com>
2023-01-02 10:11:43 +0900

    [Silver V] Title: 배열 합치기, Time: 2300 ms, Memory: 186120 KB -BaekjoonHub

--- 백준/Silver/11728. 배열 합치기/배열 합치기.py
+++ 백준/Silver/11728. 배열 합치기/배열 합치기.py
@@ -6,23 +6,20 @@ a = list(map(int, input().split()))
 b = list(map(int, input().split()))
 
 i, j = 0, 0
-ans = []
 while i < n and j < m:
     if a[i] < b[j]:
-        ans.append(a[i])
+        print(a[i], end=" ")
         i += 1
     
     else:
-        ans.append(b[j])
+        print(b[j], end=" ")
         j += 1
 
 while i < n:
-    ans.append(a[i])
+    print(a[i], end=" ")
     i += 1
 
 while j < m:
-    ans.append(b[j])
+    print(b[j], end=" ")
     j += 1
 
-for num in ans:
-    print(num, end=" ")
