a421deacc14dbc05667fb4beb103025a5607f8c5
Yun, Hong In <akenfn513@gmail.com>
2023-01-03 15:29:10 +0900

    [Gold V] Title: 인구 이동, Time: 92 ms, Memory: 2188 KB -BaekjoonHub

--- 백준/Gold/16234. 인구 이동/인구 이동.cc
+++ 백준/Gold/16234. 인구 이동/인구 이동.cc
@@ -1,73 +1,89 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int n, l, r, cnt, sum;
+int n, l, r, sum, cnt;
 int grid[51][51];
 int visited[51][51];
-vector<pair<int, int>> v;
 int dx[] = {1, -1, 0, 0};
 int dy[] = {0, 0, 1, -1};
-void dfs(int row, int col, vector<pair<int, int>> &v){
-    // dfs 순회 하면서 국경열린 곳 인구 모두 더하기
-    // 인접한 두 칸의 인구 차이가 l <= p <= r이면 개방
-    for(int i = 0; i < 4; i++){
+vector<pair<int, int>> v;
+
+void dfs(int row, int col, vector<pair<int, int>> &v)
+{
+    // row, col은 이번에 방문하는 곳 좌표
+    // v는 지금까지 방문헀던 좌표 기록하는 벡터
+    // grid 범위 안에서 l이상 r이하 차이가 나는 곳들의 모든 합을 구함 -> sum
+    // sum을 v의 길이로 나누면 인구 이동 후 같아질 인구
+    for (int i = 0; i < 4; i++)
+    {
         int nr = row + dx[i];
         int nc = col + dy[i];
-        int sub = abs(grid[row][col] - grid[nr][nc]);
+        int sub = abs(grid[nr][nc] - grid[row][col]);
 
-        if(nr < 0 || nc < 0 || nr >= n || nc >= n) continue;
-        if(visited[nr][nc] == 1) continue;
-        if(sub > r || sub < l) continue;
+        if (nr < 0 || nc < 0 || nr >= n || nc >= n)
+            continue;
+        if (visited[nr][nc])
+            continue;
+        if (sub > r || sub < l)
+            continue;
 
         visited[nr][nc] = 1;
-        v.push_back({nr, nc}); // 이번 카운트에 방문한 칸
+        v.push_back({nr, nc});
         sum += grid[nr][nc];
         dfs(nr, nc, v);
     }
 }
 
-int main() {
+int main()
+{
     ios::sync_with_stdio(0);
     cin.tie(NULL);
 
     cin >> n >> l >> r;
-    // 초기값 입력
-    for(int i = 0; i < n; i++){
-        for(int j = 0; j < n; j++){
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
             cin >> grid[i][j];
         }
     }
 
+    // visited를 초기화
     // 모든 칸을 탐색
-    // dfs함수 종료되면 visited 1인 칸을 찾아 gird에 인구 평균 입력
-    // 이때 cnt++
-    // visited 초기화 
-    while (1)
+    // dfs함수 종료되면 방문한 좌표들은 모두 계산된 인구로 변화
+    // 인구이동이 일어날 때 마다 cnt++
+
+    while (true)
     {
         bool flag = 0;
-        fill(&visited[0][0], &visited[0][0] + 51 * 51, 0);
-        for(int i = 0; i < n; i++){
-            for(int j = 0; j < n; j++){
-                if(!visited[i][j]){
-                    v.clear();
+        fill(&visited[0][0], &visited[0][0] + 51 * 51, 0); // visited 초기화
+        for (int i = 0; i < n; i++)
+        {
+            for (int j = 0; j < n; j++)
+            {
+                if (!visited[i][j]) // 새로 방문하는 점
+                {
+                    v.clear(); // 방문한 점 기록하는 벡터 초기화
                     visited[i][j] = 1;
-                    sum = grid[i][j];    // 첫 칸에서 시작하므로 sum값도 미리더해줘
-                    v.push_back({i, j}); // 여기도 마찬가지
+                    sum = grid[i][j];
+                    v.push_back({i, j});
                     dfs(i, j, v);
 
-                    // 인구이동 적용
-                    if(v.size() == 1) continue;
-                    for(pair<int, int> n : v){
-                        grid[n.first][n.second] = sum / v.size();
-                        flag = 1;
-                    }  
+                    if (v.size() == 1)
+                        continue;
+                    for (pair<int, int> p : v)
+                    {
+                        grid[p.first][p.second] = sum / v.size();
+                        flag = 1; // 인구이동 있었음 표시
+                    }
                 }
             }
         }
-        if(!flag) break; // 더이상 이동 못하면 종료
+        if (!flag)
+            break; // 더이상 이동이 없으면 끝
         cnt++;
     }
-    
-    cout << cnt << "\n";
+
+    cout << cnt;
     return 0;
 }
