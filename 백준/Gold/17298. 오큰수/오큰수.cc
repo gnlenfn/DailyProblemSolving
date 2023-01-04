27b4949600cfe0d074a0a22d4116bea7ebf1face
Yun, Hong In <akenfn513@gmail.com>
2023-01-04 10:57:44 +0900

    [Gold IV] Title: 오큰수, Time: 248 ms, Memory: 14112 KB -BaekjoonHub

--- 백준/Gold/17298. 오큰수/오큰수.cc
+++ 백준/Gold/17298. 오큰수/오큰수.cc
@@ -2,8 +2,8 @@
 using namespace std;
 
 int n;
+int nums[1000005], ret[1000005];
 stack<int> stk;
-int a[1000001], ret[1000001];
 int main()
 {
     ios::sync_with_stdio(0);
@@ -13,10 +13,10 @@ int main()
     memset(ret, -1, sizeof(ret));
     for (int i = 0; i < n; i++)
     {
-        cin >> a[i];
-        while (stk.size() && a[stk.top()] < a[i])
+        cin >> nums[i];
+        while (stk.size() && nums[stk.top()] < nums[i])
         {
-            ret[stk.top()] = a[i];
+            ret[stk.top()] = nums[i];
             stk.pop();
         }
         stk.push(i);
