b701459b4f4e7aec7a93978df2d5f907882c478d
Yun, Hong In <akenfn513@gmail.com>
2023-01-19 13:30:49 +0900

    [Silver I] Title: 미로 탐색, Time: 0 ms, Memory: 2108 KB -BaekjoonHub

--- 백준/Silver/2178. 미로 탐색/미로 탐색.cc
+++ 백준/Silver/2178. 미로 탐색/미로 탐색.cc
@@ -2,42 +2,52 @@
 using namespace std;
 
 int n, m;
-int arr[104][104];
-int visited[104][104];
-const int dx[] = {1, -1, 0, 0};
-const int dy[] = {0, 0, 1, -1};
-int main() {
-    ios::sync_with_stdio(0);
-    cin.tie(NULL);
-    cin >> n >> m;
-    
-    for(int i = 1; i < n + 1; i++){
-        string s;
-        cin >> s;
-        for(int j = 0; j < m; j++)
-            arr[i][j+1] = s[j] - '0';
-    }
+int mat[105][105];
+int visited[105][105];
+int dx[4] = {1, -1, 0, 0};
+int dy[4] = {0, 0, 1, -1};
 
+void bfs(int row, int col)
+{
     queue<pair<int, int>> q;
-    q.push({1, 1});
-    visited[1][1] = 1;
-    while(!q.empty()){
+    q.push({row, col}); // 시작 좌표 입력, 이 문제는 (1, 1) 고정
+    visited[row][col] = 1;
+
+    while (!q.empty())
+    {
         int x, y;
         tie(x, y) = q.front();
-        q.pop();
+        q.pop(); // 큐 맨 앞 꺼내기
 
-        for(int i = 0; i < 4; i++){
+        for (int i = 0; i < 4; i++)
+        {
             int nx = x + dx[i];
-            int ny = y + dy[i];
+            int ny = y + dy[i]; // (x, y)에서 네 방향 이동 수행
 
-            if(1 <= nx && nx < n + 1 && 1 <= ny && ny < m + 1){
-                if(!visited[nx][ny] && arr[nx][ny] == 1){
-                    visited[nx][ny] = visited[x][y] + 1;
-                    q.push({nx, ny});
-                }
-            }
+            if (nx < 1 || nx > n || ny < 1 || ny > m)
+                continue; // 좌표 범위 초과
+            if (visited[nx][ny] != 0 || mat[nx][ny] == 0)
+                continue; // 이미 방문했거나 이동할 수 없는 칸
+
+            visited[nx][ny] = visited[x][y] + 1; // 방문처리 및 최단거리 기록
+            q.push({nx, ny});                    // 방문 예정 큐로 추가
         }
     }
-    cout << visited[n][m] << "\n";
+}
+
+int main()
+{
+
+    cin >> n >> m;
+    for (int i = 1; i <= n; i++)
+    {
+        for (int j = 1; j <= m; j++)
+        {
+            scanf("%1d", &mat[i][j]);
+        }
+    }
+
+    bfs(1, 1);
+    cout << visited[n][m] << endl;
     return 0;
 }
