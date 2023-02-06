6b2c9895093737fd511ae2222db000d07e60e3f1
Yun, Hong In <akenfn513@gmail.com>
2023-02-06 17:38:00 +0900

    [Silver I] Title: 카드 구매하기, Time: 132 ms, Memory: 31256 KB -BaekjoonHub

---
+++ 백준/Silver/11052. 카드 구매하기/카드 구매하기.py
@@ -0,0 +1,17 @@
+import sys
+input = sys.stdin.readline
+
+n = int(input())    
+price = [0] + list(map(int, input().split()))
+dp = [0] * (n + 1)
+dp[1] = price[1]
+dp[2] = max(price[2], price[1] * 2)
+
+
+for i in range(3, n + 1):
+    dp[i] = price[i]
+
+    for j in range(1, i // 2 + 1):
+        dp[i] = max(dp[i], dp[j] + dp[i - j])
+
+print(dp[n])
